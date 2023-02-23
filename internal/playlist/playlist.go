f1ce5c4eba2740489f9ff3ff047de4d76bf6041e
Nikolay <nickchirgin@gmail.com>
2023-02-23 22:33:54 +0300

    some functions now working

--- internal/playlist/playlist.go
+++ internal/playlist/playlist.go
@@ -10,7 +10,7 @@ import (
 
 type Playlist struct {
 	 Tracks *list.List
-	 current *list.Element
+	 Current *list.Element
 	 time int
 	 mu sync.RWMutex
 	 PlaySong chan Song 
@@ -24,20 +24,24 @@ type Song struct {
 }
 
 func NewPlaylist() Playlist {
-	return Playlist{}
+	return Playlist{Tracks: list.New(), Current: &list.Element{}, PlaySong: make(chan Song), Stop: make(chan struct{}), nextprev: make(chan bool), time: 0}
 }
 
 func (p *Playlist) Play(track Song) {
 	p.mu.RLock()
 	defer p.mu.RUnlock() 
-	ctx := context.Background()
-	_, cancel := context.WithTimeout(ctx, time.Duration(track.Duration - p.time))
-	deadline, _ := ctx.Deadline()
+	ctx, cancel := context.WithTimeout(context.Background(), time.Duration((track.Duration - p.time)) * time.Second)
 	defer cancel() 
+	deadline, _ := ctx.Deadline()
+	p.Current.Value = track
+	fmt.Println(p.Current.Value)
 	select {
 	case <-ctx.Done():
-		fmt.Println("Song expired")
-		p.Next()
+		fmt.Println("Song expired", p.Tracks.Len())
+		p.PlaySong <- p.Current.Value.(Song)
+		if p.Current.Next() != nil {
+			p.Next()
+		}
 	case data:=<-p.nextprev: 
 		if data == true {
 			p.Next()
@@ -46,6 +50,8 @@ func (p *Playlist) Play(track Song) {
 		}
 	case <-p.Stop:
 		fmt.Println(time.Until(deadline))
+	default:
+		fmt.Println("TUTU")
 	}	
 }
 
@@ -63,10 +69,11 @@ func (p *Playlist) AddSong(track Song) {
 func (p *Playlist) Next() {
 	p.mu.RLock()
 	defer p.mu.RUnlock()
+	fmt.Println("ESt kto")
 	p.time = 0
-	if p.current.Next().Value != nil {
-		p.current.Value = p.current.Next().Value
-		p.Play(p.current.Value.(Song))
+	if p.Current.Next() != nil || p.Current.Next().Value != (Song{}) {
+		p.Current.Value = p.Current.Next().Value
+		p.Play(p.Current.Value.(Song))
 	}
 }
 
@@ -74,8 +81,8 @@ func (p *Playlist) Prev() {
 	p.mu.RLock()
 	defer p.mu.RUnlock()
 	p.time = 0
-	if p.current.Next().Value != nil {
-		p.current.Value = p.current.Prev().Value
-		p.Play(p.current.Value.(Song))
+	if p.Current.Next().Value != nil {
+		p.Current.Value = p.Current.Prev().Value
+		p.Play(p.Current.Value.(Song))
 	}
 }
--- main.go
+++ main.go
@@ -8,7 +8,10 @@ import (
 
 func main() {
 	pl := playlist.NewPlaylist()
-	go pl.AddSong(playlist.Song{Title: "Yo", Duration: 10})
+	pl.Tracks.PushBack(playlist.Song{Title: "Yo", Duration: 10})
+	pl.Tracks.PushBack(playlist.Song{Title: "Demolisher", Duration: 11})
+	go pl.AddSong(playlist.Song{Title: "Hey", Duration: 15})
+	go pl.Play(pl.Tracks.Back().Value.(playlist.Song))
 	select {
 	case data:=<-pl.PlaySong:
 		fmt.Println(data)
